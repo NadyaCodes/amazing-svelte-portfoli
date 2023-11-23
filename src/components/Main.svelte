19d0705fe3a7b1e18ef515bef9690d51d55dd7ad
NadyaCodes <nadyacorscadden@gmail.com>
2023-11-23 10:20:18 -0500

    add about me section

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -6,6 +6,12 @@ let steps = [
   {name: "Ultimate Todos", icon: "fa-solid fa-list-check", href: "#"},
   {name: "Pokedex", icon: "fa-solid fa-diagram-project", href: "#"},
 ]
+
+let benefits = [
+  {name: "a self taught developer", description: "This is a description for the first thing"},
+  {name: "a product design & UX finatic", description: "The second description"},
+  {name: "an excellent communicator", description: "The third description"},
+]
 </script>
 
 <main class="flex flex-col flex-1 p-4">
@@ -50,6 +56,24 @@ let steps = [
 
 </Step>
     </div>
-
+  </section>
+  <section id="about" class="py-10 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative">
+<div class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4">
+<h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
+<h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">A bit <span class="poppins text-violet-400">about</span> me.</h3>
+</div>
+<p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">I am . . .</p>
+<div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+  {#each benefits as benefit, index}
+  <div class="flex gap-6 sm:gap-8"><p class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold">
+    0{index + 1}
+  </p>
+<div class="flex flex-col gap-6 sm:gap-8">
+  <h3 class="text-2xl sm:text-3xl md:text-5xl">{benefit.name}</h3>
+  <p>{benefit.description}</p>
+</div>
+</div>
+  {/each}
+</div>
   </section>
 </main>
