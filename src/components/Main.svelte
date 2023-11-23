64cd6fdee87521da825cbb0f903e4c5c18fa0dc8
NadyaCodes <nadyacorscadden@gmail.com>
2023-11-22 23:11:23 -0500

    add Projects section

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,5 +1,11 @@
 <script>
+  import Step from "./Step.svelte";
 
+let steps = [
+  {name: "Smoljames Store", icon: "fa-solid fa-cart-shopping", href: "#"},
+  {name: "Ultimate Todos", icon: "fa-solid fa-list-check", href: "#"},
+  {name: "Pokedex", icon: "fa-solid fa-diagram-project", href: "#"},
+]
 </script>
 
 <main class="flex flex-col flex-1 p-4">
@@ -17,5 +23,33 @@
 <div class="relative shadow-2xl grid place-items-center">
   <img src={"images/2022-11-30-Seussical-0994.jpeg"} alt="Profile Image" class="object-cover z-[2] max-h-[70vh]"/>
 </div>
+  </section>
+
+
+  <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
+    <div class="flex flex-col gap-2 text-center">
+      <h6 class="text-lg sm:text-xl md:text-2xl">A few of my creative endeavors.</h6>
+    <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+      Curious to <span class="poppins text-violet-400">see</span> my work?
+    </h3>
+    </div>
+    <a href="#" target="_blank" class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200">
+      <i class="fa-regular fa-circle-play"></i>
+      <p>Watch the video</p>
+    </a>
+    <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+<Step step={steps[0]}>
+  <p>Smoljames Store is a merchandising store created with <strong class="text-violet-400">Next.js Commerce.js Stripe & Node.js + Express.js</strong> Commerce.js is a project CMS and Stripe is used for all transaction handling.</p>
+</Step>
+<Step step={steps[1]}>
+  <p>Ultimate Tools is a Full Stack <strong class="text-violet-400">Next.js, Node.js + Express.js, Firebase</strong> CRUD application that allows users to login, manage a tidy and efficacious todo list, and persist this information across devices.</p>
+
+</Step>
+<Step step={steps[2]}>
+  <p>The Pokedex is a <strong class="text-violet-400">SvelteKit + TailwindCSS</strong> web application hosted on <strong class="text-violet-400">Netlify</strong> that consumes and caches the <strong class="text-violet-400">Pokemon API</strong> to display all Pokemon information. Gotta catch em all!</p>
+
+</Step>
+    </div>
+
   </section>
 </main>
