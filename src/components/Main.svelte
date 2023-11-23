003b7e89fa6eb4a9e6dfb7e07e577821e3feafa6
NadyaCodes <nadyacorscadden@gmail.com>
2023-11-23 10:46:51 -0500

    add footer contact section

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -19,7 +19,7 @@ let benefits = [
 <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
   <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">Hi! I'm <span class="poppins text-violet-400">Nadya</span> Corscadden <br />Full Stack <span class="poppins text-violet-400">Developer</span></h2>
 <p class="text-base sm:text-lg md:text-xl">My <span class="text-violet-400">favorite tech</span> includes JavaScript (Next.js or SvelteKit), TailwindCSS, Node.js + Express.js, PostgreSQL or Prisma/Supabase</p>
-<a href="#" target="_blank" class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer">
+<a href="#" target="_blank" class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer">
   <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200">
 
   </div>
